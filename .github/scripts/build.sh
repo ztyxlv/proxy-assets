4204d8b6763a72a999b530a8739e3bdc70ea4333
ztyxlv <ztyxlv@users.noreply.github.com>
2026-03-19 22:23:07 +0800

    Updated on 2026-03-19 at 22:20:30

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -31,10 +31,10 @@ install_mihomo() {
 install_singbox() {
   local url
 
+  rm -rf /tmp/sing-box
+
   url=$(curl -fsSL https://api.github.com/repos/SagerNet/sing-box/releases/latest \
     | yq -r '.assets[] | select(.name | test("sing-box-[0-9.]+-linux-amd64\\.tar\\.gz$")) | .browser_download_url')
-
-  rm -rf /tmp/sing-box
 
   curl -fsSL "$url" \
     | tar -xz --strip-components=1 --wildcards '*/sing-box' -C /tmp
